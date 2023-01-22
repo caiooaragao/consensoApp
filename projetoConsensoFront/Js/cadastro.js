490c1fba2805315116a42cba52212a93abce4bf2
caiooaragao <97796450+caiooaragao@users.noreply.github.com>
2023-01-22 14:05:30 -0300

    adicionado ajustes do javascript dos formularios de login e cadastro

--- projetoConsensoFront/Js/cadastro.js
+++ projetoConsensoFront/Js/cadastro.js
@@ -19,27 +19,29 @@ form.addEventListener("submit", (e) => {
 })
 
 
-var verificador = 0
-var lastchildVerficador
+var verificadorNome = 0
+var verificadorEmail = 0
+var verificadorSenha = 0
+
 
 document.getElementById("inputNome").addEventListener("focus", function() {
     this.value = "";
     nome.classList.remove("is-invalid")
     divNome.removeChild(itemNome)
-    verificador = 0
-    console.log(verificador)
+    verificadorNome = 0
+    
 });
 
 document.getElementById("inputEmail").addEventListener("focus", function() {
     this.value = "";
-    verificador = 0
+    verificadorEmail = 0
     email.classList.remove("is-invalid")
     divEmail.removeChild(itemEmail)
 });
 
 document.getElementById("inputSenha").addEventListener("focus", function() {
     this.value = "";
-    verificador = 0
+    verificadorSenha = 0
     senha.classList.remove("is-invalid")
     divSenha.removeChild(itemSenha)
 });
@@ -52,40 +54,40 @@ function validarEntradas(n, e, s) {
 
 
     if (nomeValue === "" || nomeValue == null) {
-        if(verificador == 0){
+        if(verificadorNome == 0){
             nome.className = "form-control is-invalid"
             let itemNome = document.createElement("p")
             itemNome.id = "itemNome"
             divNome.appendChild(itemNome)
             itemNome.innerHTML="O campo Nome obrigatório"
             itemNome.style ="color:red; text-align:left;"
-            verificador = 1
-            console.log(verificador)
+            verificadorNome = 1
+            console.log(verificadorNome)
         }
             
     } else if (emailValue === "" || emailValue == null) {
-        if (verificador == 0){
+        if (verificadorEmail == 0){
             email.className ="form-control is-invalid"
             let itemEmail = document.createElement("p")
             itemEmail.id ="itemEmail"
             divEmail.appendChild(itemEmail)
             itemEmail.innerHTML ="Email é um campo obrigatório"
             itemEmail.style ="text-align:left; color:red;"
-            verificador = 1
+            verificadorEmail = 1
             
         }
         
 
-    } if (validarEmail(emailValue) === false) {
+    } else if (validarEmail(emailValue) === false) {
         console.log("Formato de email inválido")
-        if (verificador == 0){
+        if (verificadorEmail == 0){
             email.className ="form-control is-invalid"
             let itemEmail = document.createElement("p")
             itemEmail.id ="itemEmail"
             divEmail.appendChild(itemEmail)
             itemEmail.innerHTML ="Formato de Email inválido"
             itemEmail.style ="text-align:left; color:red;"
-            verificador = 1
+            verificadorEmail = 1
             
         }
         
@@ -93,32 +95,32 @@ function validarEntradas(n, e, s) {
     } else if (senhaValue === "" || senhaValue == null) {
         console.log("Senha é um campo obrigatório")
 
-        if (verificador == 0){
+        if (verificadorSenha == 0){
             senha.className ="form-control is-invalid"
             var itemSenha = document.createElement("p")
             itemSenha.id ="itemSenha"
             divSenha.appendChild(itemSenha)
             itemSenha.innerHTML ="Senha é um campo obrigatório"
             itemSenha.style ="text-align:left; color:red;"
-            verificador = 1
+            verificadorSenha = 1
             
         }
 
-    } else if (senhaValue.length < 8) {
+    } else if (senhaValue.length < 6) {
         console.log("A senha deve ter no mínimo 6 caracteres.")
 
-        if (verificador == 0){
+        if (verificadorSenha == 0){
             senha.className ="form-control is-invalid"
             var itemSenha = document.createElement("p")
             itemSenha.id ="itemSenha"
             divSenha.appendChild(itemSenha)
             itemSenha.innerHTML ="A senha deve ter no mínimo 6 caracteres."
             itemSenha.style ="text-align:left; color:red;"
-            verificador = 1
+            verificadorSenha = 1
             
         }
     } else {
-        console.log("Cadastro realizado com sucesso")
+        window.alert("cadastro realizado com sucesso!")
     }
 
 }
