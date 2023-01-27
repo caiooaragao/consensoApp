fd9ecf1531cf187876ba98d396f246b18ec2367c
caiooaragao <97796450+caiooaragao@users.noreply.github.com>
2023-01-27 11:54:58 -0300

    alteracao

--- projetoConsensoFront/Js/agendarServico.js
+++ projetoConsensoFront/Js/agendarServico.js
@@ -1,11 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
     // code to be executed when the DOM is ready
     getServicosDisponiveis()
-    
+    agendarServico()
 
 
 
 }) 
+
+
+
+function agendarServico() {
+    
+
+
+    (async () => {
+        const rawResponse = await fetch('http://localhost:8080/agendamento', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({nome: nome, email:email, senha:senha, tipoUsuario:{
+            idTipoUsuario:tipoUsuario
+          }})
+        });
+        const content = await rawResponse.json();
+      
+        console.log(content);
+      })();
+  }
 
 
 async function getServicosDisponiveis() {
